111afe2ab63a3f7c640306bae8ecd579b28cda43
Shubham Kumar <project@fathommarineconsultants.com>
2025-12-02 10:20:44 +0530

    Built login form structure with functionality

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -1,21 +1,24 @@
-import React,{useState, type FormEvent} from 'react'
-
+import React, { useState, type FormEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
 type LoginForm = {
     email: string
     password: string
 }
 const Login: React.FC = () => {
     const [form, setForm] = useState<LoginForm>({
-        email:"",
-        password:"",
+        email: "",
+        password: "",
     })
-    const [error, setError] = useState<string|null>(null);
 
-    const handleChange(e: React.ChangeEvent<HTMLInputElement>)=>{
-        const {name, value}= e.target;
-        setForm(prev=>({...prev,[name]:value}));
+    const navigate = useNavigate();
+
+    const [error, setError] = useState<string | null>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setForm(prev => ({ ...prev, [name]: value }));
     }
-    const handleSubmit=(e: React.FormEvent)=>{
+    const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         console.log(form);
     }
@@ -23,31 +26,36 @@ const Login: React.FC = () => {
     return (
         <div>
             <div>
-            <h1>Log in</h1>
-            
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <div>
+                <h1>Log in</h1>
+                <h2>Welcome back! Please enter your details</h2>
+                <div>
+                    <form onSubmit={handleSubmit}>
                         <div>
-                            <label>Email</label>
-                            <input type='text' name='email' value={form.email} onChange={handleChange} />
-                            <label>Password</label>
-                            <input type='text' name='password' value={form.password} onChange={handleChange} />
-                        </div>
-                        <div>
-                            <button>Sign In</button>
-                        </div>
-                        <div>
-                            <span><button>Forgot password? </button></span>
-                        </div>
-                        <div>
-                            <div>Not a user?
-                                <span><button>Register</button></span>
+                            <div>
+                                <label>Email</label>
+                                <input type='text' name='email' value={form.email} onChange={handleChange} placeholder='Enter your email'/>
+                                <label>Password</label>
+                                
+                                <input type='text' name='password' value={form.password} onChange={handleChange} placeholder='Enter your password'/>
+                                <button type='button' onClick={()=>navigate('/forgot-password')}>Forgot password ?</button>
+                            </div>
+                            <div>
+                                <button>Log In</button>
+                            </div>
+                            <div>
+                                <h2>OR</h2>
+                                <button>Login with Google</button>
+                            </div>
+                            <div>
+                                <div>Don't have an account?
+                                    <span>
+                                        <button onClick={()=>navigate('/register')}>Sign up</button>
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
